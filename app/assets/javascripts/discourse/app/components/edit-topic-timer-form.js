ssangyongsports/65342514

Reorder options on the topic timer modal

app/assets/javascripts/discourse/app/components/edit-topic-timer-form.js
@@ -17,7 +17,6 @@ import { isEmpty } from "@ember/utils";
 import KeyboardShortcuts from "discourse/lib/keyboard-shortcuts";
 import {
   TIME_SHORTCUT_TYPES,
-  defaultShortcutOptions,
   shortcutOptions,
 } from "discourse/lib/time-shortcut";
 import ItsATrap from "@discourse/itsatrap";
@@ -86,13 +85,18 @@ export default Component.extend({
   @discourseComputed()
   timeOptions() {
     const timezone = this.currentUser.resolvedTimezone(this.currentUser);
-    const optionsFactory = shortcutOptions(timezone);
+    const options = shortcutOptions(timezone);
 
-    const options = defaultShortcutOptions(timezone);
-    options.push(optionsFactory.twoWeeks());
-    options.push(optionsFactory.sixMonths());
-
-    return options;
+    return [
+      options.laterToday(),
+      options.tomorrow(),
+      options.laterThisWeek(),
+      options.thisWeekend(),
+      options.monday(),
+      options.twoWeeks(),
+      options.nextMonth(),
+      options.sixMonths(),
+    ];
   },
 
   @discourseComputed

Unacademy/discourse

app/assets/javascripts/discourse/app/components/edit-topic-timer-form.js
@@ -17,7 +17,6 @@ import { isEmpty } from "@ember/utils";
 import KeyboardShortcuts from "discourse/lib/keyboard-shortcuts";
 import {
   TIME_SHORTCUT_TYPES,
-  defaultTimeShortcuts,
   timeShortcuts,
 } from "discourse/lib/time-shortcut";
 import ItsATrap from "@discourse/itsatrap";
@@ -88,10 +87,16 @@ export default Component.extend({
     const timezone = this.currentUser.resolvedTimezone(this.currentUser);
     const shortcuts = timeShortcuts(timezone);
 
-    const options = defaultTimeShortcuts(timezone);
-    options.push(shortcuts.twoWeeks());
-    options.push(shortcuts.sixMonths());
-    return options;
+    return [
+      shortcuts.laterToday(),
+      shortcuts.tomorrow(),
+      shortcuts.laterThisWeek(),
+      shortcuts.thisWeekend(),
+      shortcuts.monday(),
+      shortcuts.twoWeeks(),
+      shortcuts.nextMonth(),
+      shortcuts.sixMonths(),
+    ];
   },
 
   @discourseComputed